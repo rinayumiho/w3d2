48e3933b96b3c14e5bb54ebbae0663099224ee50
gamestar <rinayumiho@gmail.com>
2021-06-29 14:39:37 -0400

    Long_round_2

--- memory_puzzle/board.rb
+++ memory_puzzle/board.rb
@@ -1,6 +1,24 @@
 class Board
+    @@alpha = ("A".."Z").to_a
+
+    def self.get_symbols(num)
+        symbols = @@alpha.sample(num)
+        #[A, B]
+        symbols += symbols
+    end
+
+    def self.create_card(symbols)
+
+    end
+
     def initialize(size)
         @grid = Array.new(size) { Array.new(size,"*") }
-        
+        @cards = []
+        symbols = Board.get_symbols(size * size / 2)
+
+    end
+
+    def []=()
+
     end
 end
