20ed1a52758fde7026009223465274c4b341073d
cindyjiang <cindyj301@gmail.com>
2021-06-29 14:14:28 -0500

    board cont.

--- memory_puzzle/board.rb
+++ memory_puzzle/board.rb
@@ -1,24 +1,47 @@
+require_relative "card.rb"
+
 class Board
     @@alpha = ("A".."Z").to_a
 
     def self.get_symbols(num)
         symbols = @@alpha.sample(num)
-        #[A, B]
+        #[A, B, A, B]
         symbols += symbols
-    end
-
-    def self.create_card(symbols)
-
+        symbols.shuffle!
     end
 
     def initialize(size)
         @grid = Array.new(size) { Array.new(size,"*") }
-        @cards = []
+        @cards = [] #[A, B, A, B]
         symbols = Board.get_symbols(size * size / 2)
 
+        while symbols.empty?
+            @cards << Card.new(symbols.pop)
+        end
     end
 
-    def []=()
-
+    def populate
+        index = 0
+        (0...@grid.size).each do |i|
+            (0...@grid.size).each do |j|
+                @grid[i][j] = @cards[index]
+                index += 1
+            end
+        end
     end
+
+    def render 
+        @grid.each do |array|
+            str = ""
+            array.each do |card|
+                if card.face_up
+                    str += card.face_value + " "
+                else
+                    str += "* "
+                end
+            end
+            puts str
+        end
+    end
+
 end
